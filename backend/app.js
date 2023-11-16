02d690f6721e3fb89ae8b69918077b7b50bedb55
Tungaamaa Gerel <tungaa@Tungaamaas-MacBook-Air.local>
2023-11-15 20:58:29 -0800

    added env and gitignore

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@
 
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
-const port =8000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("hello world! checking");
