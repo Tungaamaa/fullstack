f5cc9db1b6dd1936b136fb6afd7d2fd3ef5fa38d
Tungaamaa Gerel <tungaa@Tungaamaas-MacBook-Air.local>
2023-11-15 20:24:13 -0800

    initialized express app

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+
+
+const express = require("express");
+const app = express();
+
+const port =8000;
+
+app.get("/", (req, res) => {
+    res.send("hello world! checking");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+});;
