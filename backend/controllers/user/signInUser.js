bb0940f477ac2a91243ce6183bb029622752a712
Tungaamaa Gerel <tungaa@Tungaamaas-MacBook-Air.local>
2023-12-08 15:30:00 -0800

    added token

--- backend/controllers/user/signInUser.js
+++ backend/controllers/user/signInUser.js
@@ -1,8 +1,43 @@
 const mongoose = require("mongoose");
 const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const signInUser = (req, res) => {
-    res.send("sign in user");
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+}
+
+
+const signInUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send("please provide a valid email and password");
+    return;
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res
+      .status(400)
+      .send("User does not exist! Check your email address and password");
+    return;
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    res.status(400).send("Invalid credentials");
+    return;
+  }
+
+  const token = createToken(user._id);
+
+  res.status(200).json({ message: "Sign in successfully", user });
 };
 
 module.exports = { signInUser };
+
+//"email":"test1@gmail.com",
+//"password":"123@*Tljkjk"
--- backend/controllers/user/signUpUser.js
+++ backend/controllers/user/signUpUser.js
@@ -1,8 +1,51 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../../models/user");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
-const signUpUser = (req, res) => {
-    res.send("sign up user");
+const signUpUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send("please provide a valid email and password");
+    return;
+  }
+
+  if (!validator.isEmail(email)) {
+    res.status(400).send("Please provide a valid email");
+    return;
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    res.status(400).send("Please provide a strong password");
+    return;
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(400).send("User already exists");
+      return;
+    }
+
+    //salting and hashing password
+    const salt = await bcrypt.genSalt(10);
+    //hasging the password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({ email, password: hashedPassword });
+    const token = createToken(newUser._id);
+
+    res.send(200).json({ newUser });
+  } catch (error) {
+    res.status(500).send(error.message);
+    return;
+  }
 };
 
 module.exports = { signUpUser };
